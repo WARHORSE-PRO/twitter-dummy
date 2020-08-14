4e903db8a21085dcada85309c84e96485b8264c5
WARHORSE-PRO <clinton@fytinnovations.com>
2020-08-14 21:48:05 +0530

    add code to repository

---
+++ app/Http/Controllers/ExploreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class ExploreController extends Controller
+{
+    //
+    public function __invoke()
+    {
+    	return view('explore', [
+            'users' => User::paginate(50),
+        ]);
+    }
+
+
+}
